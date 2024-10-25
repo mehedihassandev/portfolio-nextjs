f28c0dc19f8c472f7f637824332150eea50804ea
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-10-25 11:11:43 +0600

    PN-00: Add skills section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Achievement from "./(pages)/_home/achievement";
 import Hero from "./(pages)/_home/hero";
+import Skills from "./(pages)/_home/skills";
 
 export default function Home() {
     return (
         <>
             <Hero />
             <Achievement />
+            <Skills />
         </>
     );
 }
