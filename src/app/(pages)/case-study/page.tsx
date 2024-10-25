4e5d5c9ca382380bbc7f55e5e1a3f5015778d50a
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-10-25 17:49:21 +0600

    PN-00: Add CaseStudy component to home page

---
+++ src/app/(pages)/case-study/page.tsx
@@ -0,0 +1,19 @@
+export const CaseStudy = () => {
+    return (
+        <section className="mt-24">
+            <div className="px-5 lg:px-24 mx-auto max-w-screen-2xl">
+                <div className="flex items-center justify-between">
+                    <div className="flex flex-col overflow-hidden">
+                        <h2 className="text-5xl text-gray">Project</h2>
+                        <p className="text-2xl pt-4">App Design</p>
+                    </div>
+                    <h4 className="text-black_primary font-bold text-6xl">
+                        MyCare - Online Wallet App
+                    </h4>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default CaseStudy;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
                 primary: "#0DB760",
                 case_study: "#FAFAFA",
                 text_color: "#323232",
+                gray: "#8D9395",
+                dark_gray: "#323232",
+                black_primary: "#071518",
             },
             fontFamily: {
                 inter: ["Inter", "sans-serif"],
