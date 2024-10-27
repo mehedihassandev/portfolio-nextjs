f41685956b6a4b6ee44a314232f8624a3b46653f
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-10-27 12:19:49 +0600

    main: Refactor import and image paths in about and home components

--- src/app/(pages)/_home/about.tsx
+++ src/app/(pages)/_home/about.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function AboutPage() {
+export default function About() {
     return (
         <section>
             <Image
--- src/app/(pages)/_home/page.tsx
+++ src/app/(pages)/_home/page.tsx
@@ -1,10 +1,10 @@
-import About from "../about/page";
 import Achievement from "./achievement";
 import Portfolio from "./portfolio";
 import Hero from "./hero";
 import Project from "./project";
 import Skills from "./skills";
 import Slider from "./slider";
+import About from "./about";
 
 export default function HomePage() {
     return (
