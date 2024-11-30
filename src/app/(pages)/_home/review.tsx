037e63763598acbcab6657cb3b3174ddd55e9f9c
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-11-30 15:12:05 +0600

    Add slick-carousel styles to layout and remove unused imports from review component

--- src/app/(pages)/_home/review.tsx
+++ src/app/(pages)/_home/review.tsx
@@ -2,8 +2,6 @@
 
 import { useRef } from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 import ReviewCard from "@/app/_components/review-card";
 import { reviews } from "@/app/_constant/review";
